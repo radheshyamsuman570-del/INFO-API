8c18b5afd63ad66a439087e329fd538e371c2c6d
radheshyamsuman570-del <radheshyamsuman570@gmail.com>
2026-09-13 17:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def extract_token_from_response(data, region):
 def get_jwt_token_sync(region):
     global jwt_token
     endpoints = {
-        "IND": "https://jwtgenchx.vercel.app/token?uid=3976277520&password=3C07CA0CF3C22DB4DB5A00A8C75E3FED7869FB11CED0ADFB5C8DE7E92652B704",
+        "IND": "https://jwtgenchx.vercel.app/token?uid=7664286019&password=351511A27F7631D3D07AD877D0DC17FF0C72DD45E6596EF6CE02C986295DC3F4",
         "BR": "https://projects-fox-x-get-jwt.vercel.app/get?uid=3787481313&password=JlOivPeosauV0l9SG6gwK39lH3x2kJkO",
         "US": "https://tokenalljwt.onrender.com/api/oauth_guest?uid=3787481313&password=JlOivPeosauV0l9SG6gwK39lH3x2kJkO",
         "SAC": "https://tokenalljwt.onrender.com/api/oauth_guest?uid=3787481313&password=JlOivPeosauV0l9SG6gwK39lH3x2kJkO",
